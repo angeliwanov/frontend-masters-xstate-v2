663451649f1d51a9b031bafe8e412159744f2ac8
Angel Ivanov <angel.iwanov@gmail.com>
2024-04-05 16:52:10 +0300

    state modeling

--- 00-modeling/main.js
+++ 00-modeling/main.js
@@ -6,3 +6,88 @@ import '../style.css';
 
 // Also, come up with a simple way to "interpret" it, and
 // make it an object that you can `.send(...)` events to.
+
+const reducer = (initialState, event) => {
+  switch (event.type) {
+    case 'LOADED':
+      if (initialState.status === 'loading') return {status: 'playing',...initialState}
+    case 'PLAY':
+      if (initialState.status === 'paused') return {status: 'playing', ...initialState} 
+    case 'PAUSE':
+      if (initialState.status === 'playing') return {status: 'paused', ...initialState}
+    default:
+      break
+  }
+
+  return {...initialState}
+}
+
+function transition(state = initialState, event) {
+  switch (state.value) {
+    case 'loading':
+      if (event.type === 'LOADED') return {...state, value: 'playing'}
+      break;
+    case 'playing':
+      if (event.type === 'PAUSE') return {...state, value: 'paused'}
+      break;
+    case 'paused':
+      if (event.type === 'PLAY') return {... state, value: 'playing'}
+      break;
+    default:
+      break
+  }
+
+  return state;
+}
+
+const initialState = {value: 'loading'}
+
+window.transition = transition
+
+const machine = {
+  initial: 'loading',
+  states: {
+    loading: {
+      on: {
+        LOADED: 'playing'
+      }
+    },
+    playing: {
+      on: {
+        PAUSE: 'paused'
+      }
+    },
+    paused: {
+      on: {
+        PLAY: 'playing'
+      }
+    }
+  }
+}
+
+function machineTransition(state = {value: machine.initial}, event) {
+  const nextStateValue =
+    machine.states[state.value].on?.[event.type];
+
+  if (!nextStateValue) {
+    return state;
+  }
+
+  return {
+    ...state,
+    value: nextStateValue,
+  };
+}
+
+window.machineTransition = machineTransition
+
+let currentState = {value: machine.initial}
+
+const service = {
+  send: (event) => {
+    currentState = machineTransition(currentState, event);
+    console.log(currentState)
+  }
+}
+
+window.service = service;
--- main.js
+++ main.js
@@ -1,2 +1,41 @@
 import './style.css';
-import { createMachine } from 'xstate';
+
+//state {status: 'idle'}
+//envet: {type: 'FETCH'}
+function transition(state, event) {
+  switch (state.status) {
+    case 'idle':
+      if (event.type === 'FETCH') {
+        console.log('Starting ot the fetch data');
+        return {status : 'loading'}
+      }
+      return state
+    case 'loading':
+      break;
+    default:
+      break
+  }
+  return state;
+}
+
+const machine = {
+  initial: 'idle',
+  states: {
+    idle: {
+      FETCH: 'loading'
+    },
+    loading: {}
+  }
+}
+
+function transition2(state, event){
+  const nextStatus = machine.states[state.status].on?.[event.type] ?? state.status;
+
+  return {
+    status: nextStatus
+  }
+}
+
+window.machine = machine;
+window.transition = transition;
+window.transition2 = transition2;
